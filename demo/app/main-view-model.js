eb64a4b5631715621d61f8616c10306bbde593b9
Marcel Joachim Kloubert <marcel.kloubert@gmx.net>
2016-05-13 20:08:07 +0200

    first working version of demo

--- demo/app/main-view-model.js
+++ demo/app/main-view-model.js
@@ -1,24 +1,46 @@
-var Observable = require("data/observable").Observable;
+// The MIT License (MIT)
+// 
+// Copyright (c) Marcel Joachim Kloubert <marcel.kloubert@gmx.net>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-function getMessage(counter) {
-    if (counter <= 0) {
-        return "Hoorraaay! You unlocked the NativeScript clicker achievement!";
-    } else {
-        return counter + " taps left";
-    }
-}
+
+var Observable = require("data/observable").Observable;
+var PayPal = require('nativescript-paypal');
 
 function createViewModel() {
-    var viewModel = new Observable();
-    viewModel.counter = 42;
-    viewModel.message = getMessage(viewModel.counter);
+    var vm = new Observable();
+    
+    vm.init = function() {
+        PayPal.addLogger(function(msg) {
+            console.log('[nativescript-paypal] ' + msg);
+        });
+        
+        PayPal.init({
+            // if you would like use the SandBox
+            // add the client ID here
+            // and change 'environment' to 0
+            
+            // clientId: '',
+            environment: 2
+        });    
+    };
 
-    viewModel.onTap = function() {
-        this.counter--;
-        this.set("message", getMessage(this.counter));
-    }
+    vm.startPayPalCheckout = function() {
+        var payment = PayPal.newPayment()
+            .setDescription('Test product')
+            .setAmount(1.23)
+            .setCurrency('EUR');
+            
+        payment.start(function(result) {
+            console.log('payment result code: ' + result.code);
+        });
+    };
 
-    return viewModel;
+    return vm;
 }
-
 exports.createViewModel = createViewModel;
